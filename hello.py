8ee325c5564e3be37cbbcbba704a86048174dc2b
geniustesda <390131208@qq.com>
2017-08-21 01:08:08 +0800

    update

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-# coding:utf-8
+#-*- coding:utf-8 -*-
 from flask import Flask, session, redirect, url_for, request, render_template, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -7,13 +7,39 @@ from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = 'True'
+
+db = SQLAlchemy(app=app)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	users = db.relationship('User', backref='role', lazy='dynamic')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
 
 class NameForm(Form):
 	name = StringField('What is your name?', validators=[Required()])
@@ -31,9 +57,13 @@ def internal_server_error(e):
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
 		session['name'] = form.name.data
 		return redirect(url_for('index'))
-	return render_template("index.html", form=form, name=session.get('name'), current_time=datetime.utcnow())
+	return render_template("index.html", form=form, name=session.get('name'),
+						   current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def welcome(name):
